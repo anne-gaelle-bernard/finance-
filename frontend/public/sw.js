b1d4fed5b01d450d81550ba97ef7320044dc39cf
Anne-gaëlle Bernard <anne-gaelle.bernard@laplateforme.io>
2026-02-16 16:39:03 +0100

    Create sw.js
    Ajouter le service worker pour permettre le mode hors ligne et l'installation PWA

---
+++ frontend/public/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'finance-tracker-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Installation du service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Retourne le cache si disponible, sinon fait la requête
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Nettoyage des anciens caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
