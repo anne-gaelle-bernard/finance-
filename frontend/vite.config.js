75976b0d93f4afeb90d87f6cf44e4de4ea1ab687
Anne-gaëlle Bernard <anne-gaelle.bernard@laplateforme.io>
2026-03-05 11:17:51 +0100

    Update icons and glob patterns in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,31 +19,21 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-maskable.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          },
-          {
             src: 'icon.svg',
             sizes: 'any',
             type: 'image/svg+xml',
             purpose: 'any'
+          },
+          {
+            src: 'icon-maskable.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'maskable'
           }
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        globPatterns: ['**/*.{js,css,html,ico,svg}'],
         runtimeCaching: [
           {
             urlPattern: /^\/api\//,
